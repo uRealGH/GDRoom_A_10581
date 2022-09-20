be3b89b2e0bee1a1415f2190f0a36e93dd6f9902
uRealGH <111778068+uRealGH@users.noreply.github.com>
2022-09-20 15:01:25 +0700

    guided room

---
+++ app/src/main/java/com/example/gdroom_a_10581/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.gdroom_a_10581
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gdroom_a_10581.room.Constant
+import com.example.gdroom_a_10581.room.Note
+import com.example.gdroom_a_10581.room.NoteAdapter
+import com.example.gdroom_a_10581.room.NoteDB
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    val db by lazy { NoteDB(this) }
+    lateinit var noteAdapter: NoteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupListener()
+        setupRecycleView()
+    }
+
+    private fun setupRecycleView() {
+        noteAdapter = NoteAdapter(arrayListOf(), object :
+        NoteAdapter.OnAdapterListener{
+            override fun onClick(note: Note) {
+                intentEdit(note.id, Constant.TYPE_READ)
+            }
+
+            override fun onUpdate(note: Note) {
+                intentEdit(note.id, Constant.TYPE_UPDATE)
+            }
+
+            override fun onDelete(note: Note) {
+                deleteDialog(note)
+            }
+        })
+        list_note.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = noteAdapter
+        }
+    }
+
+    private fun deleteDialog(note: Note){
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.apply {
+            setTitle("Confirmation")
+            setMessage("Are You Sure to delete this data From${note.title}?")
+            setNegativeButton("Cancel", DialogInterface.OnClickListener
+            { dialogInterface, i ->
+                dialogInterface.dismiss()
+            })
+            setPositiveButton("Delete", DialogInterface.OnClickListener
+            { dialogInterface, i ->
+                dialogInterface.dismiss()
+                CoroutineScope(Dispatchers.IO).launch {
+                    db.noteDao().deleteNote(note)
+                    loadData()
+                }
+            })
+        }
+        alertDialog.show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadData()
+    }
+
+    fun loadData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = db.noteDao().getNotes()
+            Log.d("MainActivity","dbResponse: $notes")
+            withContext(Dispatchers.Main){
+                noteAdapter.setData(notes)
+            }
+        }
+    }
+
+    fun setupListener() {
+        button_create.setOnClickListener{
+            intentEdit(0,Constant.TYPE_CREATE)
+        }
+    }
+
+    fun intentEdit(noteId : Int, intentType: Int) {
+        startActivity(
+            Intent(applicationContext, EditActivity::class.java)
+                .putExtra("intent_id", noteId)
+                .putExtra("intent_type", intentType)
+        )
+    }
+}
